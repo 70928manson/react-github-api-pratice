8bc07c0e3ae004a6a827860041b8232806e26f6a
Manson <manson972@yahoo.com>
2022-04-05 03:28:29 +0800

    brouseRouter deploy fix

--- src/index.js
+++ src/index.js
@@ -7,6 +7,44 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.js';
 
+//跟後端溝通 https://github.com/scps960740/react-course-resources/tree/master/ch06/bruce-be
+const express = require('express');
+const cors = require('cors');
+const path = require('path')
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Automatically allow cross-origin requests
+app.use(cors());
+
+app.get('/', function(req, res) {
+  console.log('hit');
+  // http code
+  res.status(200).sendFile(path.join(__dirname, 'public/index.html'))
+  // res.send('hello!')
+})
+
+app.get('/user/:username/repos', function(req, res) {
+  console.log('hit');
+  // http code
+  res.status(200).sendFile(path.join(__dirname, 'public/index.html'))
+  // res.send('hello!')
+})
+
+app.get('/user/:username/repos/:repo', function(req, res) {
+  console.log('hit');
+  // http code
+  res.status(200).sendFile(path.join(__dirname, 'public/index.html'))
+  // res.send('hello!')
+})
+
+app.listen(4000, () => {
+  console.log('on 4000')
+})
+
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
