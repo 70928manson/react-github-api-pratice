3bed07274e5bf51da06fa489eeff6911c436d60f
Manson <manson972@yahoo.com>
2022-04-05 16:52:12 +0800

    delete backend node code

--- src/index.js
+++ src/index.js
@@ -7,44 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.js';
 
-//跟後端溝通 https://github.com/scps960740/react-course-resources/tree/master/ch06/bruce-be
-//官方doc https://create-react-app.dev/docs/deployment/
-const express = require('express');
-const cors = require('cors');
-const path = require('path')
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-// Automatically allow cross-origin requests
-app.use(cors());
-
-app.get('/', function(req, res) {
-  console.log('hit');
-  // http code
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  // res.send('hello!')
-})
-
-app.get('/user/:username/repos', function(req, res) {
-  console.log('hit');
-  // http code
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  // res.send('hello!')
-})
-
-app.get('/user/:username/repos/:repo', function(req, res) {
-  console.log('hit');
-  // http code
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  // res.send('hello!')
-})
-
-app.listen(4000, () => {
-  console.log('on 4000')
-})
-
 
 ReactDOM.render(
   <React.StrictMode>
